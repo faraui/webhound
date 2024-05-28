de80bf4f0a69b44f4dd12e437f31e2847fa2e6fc
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-05-28 17:45:37 +0200

    Changed output file format. Making dirs
    1. ```domain_utility``` format changed to ```utility_domain```
    2. Making a directory of outputs for each unique domain

--- webhund.sh
+++ webhund.sh
@@ -31,7 +31,7 @@ sed '1,/dark-gray/d; /background_table_section/,/$d/d;
 /geolocation_map/,/geolocation_section/d; /webbug_graph/,/webbug_graphs/d;
 /Not Present\|Unknown\|unknown/,/<tr/d' | tac | \
 sed -z 's/\n//g; s/>\s*</></g; s/>,\s*</>,</g; s/>.\s*</>.</g' > \
-$(echo $UTF_DOMAIN)_netcraft.html; rm -rf ZVhfCpi*; echo "[+] netcraft" ) &
+netcraft_$UTF_DOMAIN.html; rm -rf ZVhfCpi*; echo "[+] netcraft" ) &
 
 
 #######
@@ -39,7 +39,7 @@ $(echo $UTF_DOMAIN)_netcraft.html; rm -rf ZVhfCpi*; echo "[+] netcraft" ) &
 #######
 for ((i=1; i<=99; i++)); do
     TYPE="TYPE$(printf "%02d" $i)"
-    dig $ACE_DOMAIN -t $TYPE +noall +answer >> $(echo $UTF_DOMAIN)_dig.txt
+    dig $ACE_DOMAIN -t $TYPE +noall +answer >> dig_$UTF_DOMAIN.txt
 done
 echo "[+] dig"
 
@@ -47,30 +47,30 @@ echo "[+] dig"
 #########
 # whois #
 #########
-whois -H $ACE_DOMAIN | sed '/^%/d; /^$/d' > $(echo $UTF_DOMAIN)_whois.txt
+whois -H $ACE_DOMAIN | sed '/^%/d; /^$/d' > whois_$UTF_DOMAIN.txt
 echo "[+] whois"
 
 
 ###########
 # wafw00f #
 ###########
-sudo wafw00f -a -v -o $(echo $UTF_DOMAIN)_wafw00f.txt $ACE_DOMAIN 2>/dev/null | \
-grep -A 10000 'ing Toolkit' | sed '1d' > $(echo $UTF_DOMAIN)_wafw00f.txt
+sudo wafw00f -a -v -o wafw00f_$UTF_DOMAIN.txt $ACE_DOMAIN 2>/dev/null | \
+grep -A 10000 'ing Toolkit' | sed '1d' > wafw00f_$UTF_DOMAIN.txt
 echo "[+] wafw00f"
 
 
 ###########
 # whatweb #
 ###########
-whatweb -a 1 $URL  --log-brief=$(echo $UTF_DOMAIN)_whatweb.txt >/dev/null
+whatweb -a 1 $URL  --log-brief=whatweb_$UTF_DOMAIN.txt >/dev/null
 echo "[+] whatweb"
 
 
 ##########
 # katana #
 ##########
-katana -u $URL -o $(echo $UTF_DOMAIN)_katana.txt >/dev/null 2>&1
-cp $(echo $UTF_DOMAIN)_katana.txt $(echo $UTF_DOMAIN)_urls_temp.txt 
+katana -u $URL -o katana_$UTF_DOMAIN.txt >/dev/null 2>&1
+cp katana_$UTF_DOMAIN.txt urls-tmp_$UTF_DOMAIN.txt 
 echo "[+] katana"
 
 
@@ -78,15 +78,20 @@ echo "[+] katana"
 # dirsearch #
 #############
 echo '[ ] dirsearch in progress'
-dirsearch -u $URL -o $(echo $UTF_DOMAIN)_dirsearch.txt -t 3 >/dev/null 2>&1
-cat $(echo $UTF_DOMAIN)_dirsearch.txt | tr -s ' ' | cut -f 3 -d ' ' >> $(echo $UTF_DOMAIN)_urls_temp.txt
-cat $(echo $UTF_DOMAIN)_urls_temp.txt | sort -u | rg http  > $(echo $UTF_DOMAIN)_urls.txt; rm $(echo $UTF_DOMAIN)_urls_temp.txt 
+dirsearch -u $URL -o dirsearch_$UTF_DOMAIN.txt -t 3 >/dev/null 2>&1
+cat dirsearch_$UTF_DOMAIN.txt | tr -s ' ' | cut -f 3 -d ' ' >> urls-tmp_$UTF_DOMAIN.txt
+cat urls-tmp_$UTF_DOMAIN.txt | sort -u | rg http  > urls_$UTF_DOMAIN.txt; rm urls-tmp*.txt 
 echo "[+] dirsearch"
 
 ######
 
 
-######
+files=( * )
+for file in "${files[@]}"; do
+    dir="${file#*_}"; dir="${dir%.*}"
+    mkdir -p "$dir"; mv "$file" "$dir"
+done
+mv webhund/webhund.sh .; rmdir webhund
 
 
 ######
